67a02e0af31422605dadb37fd7f29bde2f91fe00
Ask Hetland Berentsen <askberentsen@gmail.com>
2018-10-11 20:17:34 +0200

    decoupled requestResponseHandler constructor

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+                              
 /*I work 50/50 with VS-code and Atom. Indentation is correct in editor, but not necessairily in browser*/
 
 //https://api.nasa.gov/
@@ -7,94 +8,32 @@ window.onload = init;
 // xmlhttp request object for handling multiple XMLHttpRequests	//
 //////////////////////////////////////////////////////////////////
 class RequestResponseHandler{
-    /* 
-        simplified syntax
-        var object = new RequestResponseHandler(String, Function, optional Function);
-        or
-        var object = new RequestResponseHandler(Array, Function, optional Function);
 
-        complete syntax
-        var object = new RequestResponseHandler( url.JSON, function(response,index), function(responses));
         var object = new RequestResponseHandler( requestObject, function(response,index), function(responses));
-
-        Sends a list of requests and sends the responses to the responseHandler.
-        On completion of all requests, the complete list of responses is given to the completionHandler.
-
-        The completionHandler is Optional, but the responseHandler is mandatory.
-    */
     constructor(requests, responseHandler, completionHandler){
 
-        /* Initialize attributes */
+        validateArguements(requests, responseHandler, completionHandler);
+
         this.handle             = responseHandler;
         this.completionHandle   = completionHandler;
+        this.requestList        = requests;
 
-        this.requestList        = [];
         this.responses          = [];
         this.completions        = 0;
-        ////CONSTRUCTION ERROR HANDLING////
-        {
-            ////HANDLER ARG ERRORS////
-            if (!(responseHandler instanceof Function)){
-                /*arguement responseHandler is not a valid function*/
-                throw 'TypeError: responseHandler "'+ responseHandler +'" is not a function';
-            }
-            else if(completionHandler !== undefined && !(completionHandler instanceof Function)){
-                /*arguement completionHandler is defined and not a function*/
-                throw 'TypeError: completionHandler "'+ responseHandler +'" is defined, but not a function';
-            }
-
-            ////REQUEST ARG ERRORS////
-            if ( requests instanceof Array){
-                /*requests are of Array type*/
-
-                /*assign requests directly to requestList*/
-                this.requestList = requests;
-                this.request();
-            }
-            else if( typeof requests === "string" ){
-                /*requests are of string type*/
-
-                if (/\.JSON$/.test(requests)){
-                    /*requests is a JSON file*/
-
-                    this.sendRequest(requests, response => {
-        
-                        /*assign JSON list to xhttp list*/
-                        this.requestList = response;
-            
-                        /*initiate requests*/
-                        this.request();
-                    });
-                }
-                else{
-                    /*request is not a JSON file*/
-                    throw 'TypeError: could not bootstrap requests with arguement "' + requests +'" (must be a .JSON file)';
-                }
-            }
-            else {
-                /*request is something else*/
-                throw "TypeError: arguement is not a bootstrap string (to a .JSON file) or an array of requests"; 
-            }
-        }
     }
-    request(){
+
+    sendRequestList(){
         /*Iterate over requestList[i], and send asynchronous requests*/
         for( let i = 0; i < this.requestList.length; ++i){
-            
-            if (this.requestList[i].url !== undefined){
-                /*send request from requestList[]. When server responds, assign response to responses[]*/
-                this.sendRequest( this.requestList[i].url, (response)=>{this.recieve(response, i)} );
-            }
-            else{
-                throw "MissingAttributeException: requestList[" + i + "] (" + this.requestList[i] + ") is missing an url attribute";
-            }
-
+            /*send request from requestList[]. When server responds, assign response to responses[]*/
+            sendRequest( this.requestList[i].url, (response)=>{this.recieve(response, i)}, i );
         }
     }
+
     recieve( response, index ){
     
-        /*Complete response. Increment counter*/
-        ++this.completions;
+        /*Complete response. Increment completions counter*/
+        this.completions++;
         
         /*add response to responses[]*/
         this.responses[ index ] = {
@@ -103,42 +42,15 @@ class RequestResponseHandler{
         }
 
         /*send response to handler*/        
-        this.handle(this.responses[index], index);
+        this.handle(this.responses[ index ], index);
 
         /*if a completionhandler has been given, and all responses are handled, send to completionhandler*/
         if(this.completionHandle !== undefined && this.completions === this.requestList.length){
             this.completionHandle(this.responses);
         }
     }
-    sendRequest(request, handle, index){
-
-        /*Define new request*/
-        let  requestObject = new XMLHttpRequest();
-    
-        /*Define handler for request completion*/
-        requestObject.onload = () => {
-    
-            /*Response is OK*/
-            if ( requestObject.status === 200 ){
-    
-                /*Handle response*/
-                handle( JSON.parse(requestObject.responseText), index );
-            }
-    
-            /*Response is not ok*/
-            else {
-    
-                /*Handle null for failed response*/
-                handle( null, index );
-            }
-        }
-        
-        /*send request*/
-        requestObject.open("GET", request, true);
-        requestObject.send();
-        return requestObject;
-    }
 }
+
 class RequestObject{
     constructor(url, domain, info = {}){
         this.info = info;
@@ -147,14 +59,75 @@ class RequestObject{
     }
 }
 
+function validateArguements(requestObject, mandatoryCallback, optionalCallback){
+
+    if (!(mandatoryCallback instanceof Function)){
+        /*arguement responseHandler is not a valid function*/
+        throw new Error('mandatory callback "'+ mandatoryCallback +'" is not a function');
+    }
+    else if(optionalCallback !== undefined && !(optionalCallback instanceof Function)){
+        /*arguement completionHandler is defined and not a function*/
+        throw new Error('optional callback "'+ optionalCallback +'" is defined, but not a function');
+    }
+    else if ( requestObject instanceof Array){
+        /*requests are of Array type*/
+
+        for( let i = 0; i < requestObject.length; ++i){
+            if (requestObject[i].url === undefined){
+                /*object is missing an url tag*/
+                throw new Error("request object[" + i + "] (" + requestObject[i] + ") is missing an url attribute");
+            }
+        }
+    }
+    else {
+        throw new Error('request object "'+requestObject+'" is not an array');
+    }
+}
+
+function sendRequest(request, handle, index){
+
+    /*Define new request*/
+    let  requestObject = new XMLHttpRequest();
+
+    /*Define handler for request completion*/
+    requestObject.onload = function(){
+
+        /*Response is OK*/
+        if ( requestObject.status === 200 ){
+
+            /*Handle response*/
+            handle( JSON.parse(requestObject.responseText), index );
+        }
+
+        /*Response is not ok*/
+        else {
+
+            handle( {
+                status: requestObject.status,
+                message: requestObject.statusText
+            }, index );
+        }
+    }
+    
+    /*send request*/
+    requestObject.open("GET", request, true);
+    requestObject.send();
+    return requestObject;
+}
+
+
 
 //////////////////////////////////////////////////////////////////
 //					Initialize requests and DOM					//
 //////////////////////////////////////////////////////////////////
 var xhttp;
+
 function init(){
-    let request = new RequestObject("https://api.spacexdata.com/v3/launches", "SpaceX");
-    xhttp = new RequestResponseHandler("webRequests.JSON", handleResponse, handleAllResponses);
+
+    sendRequest("webRequests.json", (jsonRequestList)=>{
+        xhttp = new RequestResponseHandler(jsonRequestList, handleResponse, handleAllResponses);
+        xhttp.sendRequestList();
+    })
 
 }
 
