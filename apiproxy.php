c12c8b00de1fbb3fa5b13fee087389a153e5c88c
Ask Hetland Berentsen <askberentsen@gmail.com>
2018-10-22 20:20:43 +0200

    Enabled caching with curl
    Api responses are now miles faster

--- apiproxy.php
+++ apiproxy.php
@@ -1,29 +1,67 @@
 <?php
 
-    header("Content-type: application/json");
-    $file = fopen("webRequests.json","r");
-    $content = "";
+    /* functions getContent and getUrl from https://davidwalsh.name/php-cache-function */
+    function get_content($file,$url,$hours = 24,$fn = '') {
 
-    while( $line = fgets( $file )){
-        $content .= $line;
+        //vars
+        $current_time = time(); 
+        $expire_time = $hours * 60 * 60; 
+        $file_time = filemtime($file);
+        //decisions, decisions
+        if( file_exists( "cache/" . $file ) && ($current_time - $expire_time < $file_time) ) {
+            return file_get_contents($file);
+        }
+        else {
+            $content = get_url( $url );
+            if($fn) { $content = $fn( $content ); }
+            file_put_contents( "cache/" . $file , $content);
+            return $content;
+        }
     }
-    $requests = json_decode( $content );
+    function get_url( $url ) {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+
+        $response = curl_exec( $curl );
+        $err = curl_error( $curl );
+
+        curl_close( $curl );
+
+        if ( $err ) {
+            return "cURL Error #:" . $err;
+        } else {
+            return $response;
+        }
+    }
+
+    
+
+    $rl_file = file_get_contents("webRequests.json");
+    $request_list = json_decode($rl_file);
 
     $responses = array();
 
-    foreach ( $requests as $request ){
-        $url = $request->url;
-        $file = fopen($url, "r");
+    foreach( $request_list as $request ){
 
-        $response = "";
+        $raw = get_content( $request->cache, $request->url );
 
-        while( $line = fgets( $file ) ){
-            $response .= $line;
-        }
+        $response = json_decode( $raw );
 
-        $item = array( "info"=> $request->info , "url"=> $request->url, "content"=> $response);
+        $item = array( "info"=>$request->info, "url"=> $request->url, "content"=> $response );
 
-        array_push($responses, $item );
+        array_push($responses, $item);
+        
     }
-    echo json_encode($responses);
+
+    echo json_encode( $responses );
+
 ?>
