7775b3900cce240aa398dcc747bd273a6b046bdc
Ask Hetland Berentsen <askberentsen@gmail.com>
2018-10-23 20:29:46 +0200

    Added article appendation functionality
    main.js now adds articles to index.html with rudimentary information

--- main.js
+++ main.js
@@ -47,13 +47,170 @@ function writeArticle( location, json ){
 
     var article = document.createElement("article");
 
-    var header = document.createElement("header");
+    json.content = json.info.type === "latest" ? json.content[ json.content.length - 1 ] : json.content;
 
-    header.innerHTML = json.info.domain;
+    jTitle( article, json );
 
-    article.appendChild( header );
+    jMedia( article, json );
+
+    jSummary( article, json );
+
+    //jDetails( article, json );
+
+    //jFooter( article, json );
+
+    location.appendChild(article);
+}
+
+function jTitle( article, json ){
+
+    /* Instantiate nodes */
+    var banner = document.createElement("header");
+    var title = document.createElement("h2");
+
+    /*  Extract title from json.  */
+    /*  Different API responses have different structure, so if 
+        none are found where expected, default to domain name  */
+    var titleText = 
+        json.content.title || 
+        json.content.mission_name || 
+        json.content.name || 
+        json.content.ship_name || 
+        json.info.domain;
+
+    /*  Append title to banner */
+    title.innerHTML = titleText;
+    banner.appendChild( title );
+
+    /*  Extract timestamp */
+    /*  If no timestamp is given, don't add timestamp to article */
+    var timeStamp = 
+        json.content.date || 
+        json.content.launch_date_utc || 
+        json.content.event_date_utc || 
+        json.content.year_built;
+
+    if( timeStamp ){
+
+        /* Append timestamp */
+        var time = document.createElement("time");
+        time.dateTime = timeStamp;
+
+        timeUpdater( json, timeStamp, time );
+
+        banner.appendChild( time );
+    }
+
+    /* Append title to article */
+    article.appendChild( banner );
+}
+
+/*  Write summary if available  */
+function jSummary( article, json ){
+
+    var text = json.content.details || json.content.explanation;
+
+    if ( text ){
+        var summary = document.createElement("p");
+        summary.innerHTML = text;
+        article.appendChild( summary );
+    }
+
+}
+
+/*  Add image if available  */
+function jMedia( article, json ){
+
+    var media;
+    
+    /*  Extract image url(s) from json file where expected */
+    var image = json.content.image || json.content.hdurl || json.content.url;
+    var imageArray = json.content.flickr_images || (json.content.links ? json.content.links.flickr_images : undefined );
+
+    var type = json.content.media_type;
+
+    /*  Check if json contains several images */
+    if( imageArray ){
+
+        /*  instantiate image wrapper  */
+        media = document.createElement("figure");
+
+        /*  for each image in array, append to wrapper */
+        for( var i = 0; i < imageArray.length; ++i ){
+            image = document.createElement("img");
+            image.src = imageArray[i];
+            media.appendChild( image );
+        }
+
+    }
+    /*  Check if a single image was found instead */
+    else if ( type === "image" || json.content.image ){
+
+        media = document.createElement("img");
+        media.src = image;
+
+    }
+
+    /*  If an image was found, append media to article */
+    if( media ){
+        article.appendChild( media );
+    }
+}
 
 
-    location.appendChild( article );
+function updateTime( timeStamp, dateType ){
+    var date = new Date(timeDifference(timeStamp));
+
+    var yearsLeft = date.getUTCFullYear() - 1970;
+
+    return (yearsLeft > 0 ? yearsLeft + " years, " : "") +
+        date.getUTCMonth() + " months, " +
+        date.getUTCDate() + " days, " +
+        date.getUTCHours() + " hours, " +
+        date.getUTCMinutes() + " minutes and " +
+        date.getUTCSeconds() + " seconds left" +
+        (dateType ? " until " + dateType : "");
+}
+
+function timeDifference( from, to = Date.now() ){
+    return new Date( from ) - new Date(to);
+}
+
+function initializeTime( json, timeStamp ){
+    return (
+        json.content.launch_date_utc ? "Launched: " + timeStamp : undefined ) ||
+        json.content.year_built ? "Built in " + timeStamp : undefined ||
+        "Date: " + timeStamp;
+}
+
+
+
+
+function timeUpdater( json, timeStamp, timeObject ){
+
+    /* If time is in the future update time regularly */
+    if( json.content.is_tentative || timeDifference( timeStamp ) > 0 ){
+
+        /* update on initialization */
+        timeObject.innerHTML = updateTime( timeStamp, "launch" );
+
+        /* Define an updater to be called every second */
+        var updater = setInterval( function(){
+
+            /* update time */
+            timeObject.innerHTML =  updateTime(timeStamp, "launch" );
+
+            /* If the time is in the past, set time to timeStamp and cancel updater */
+            if ( timeDifference( timeStamp ) <= 0 ){
+
+                clearInterval( updater );
+                timeObject.innerHTML = timeStamp;
+            }
+        }, 800 );
+    }
+    /* Time is in the past, initialize timeObject to timeStamp */
+    else {
+        timeObject.innerHTML = initializeTime( json, timeStamp );
+    }
 
 }
